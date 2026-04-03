1672a3e7a451a6219c13df9d430f93ff70297338
SantiagoSu15 <sasupu08@gmail.com>
2026-04-03 00:34:38 -0500

    Dto para conexion STOMP

--- src/main/java/Fight_club/Fight_Services/Infrastructure/Inbound/FightSocketController.java
+++ src/main/java/Fight_club/Fight_Services/Infrastructure/Inbound/FightSocketController.java
@@ -3,6 +3,7 @@ package Fight_club.Fight_Services.Infrastructure.Inbound;
 
 import Fight_club.Fight_Services.Application.Ports.Input.ProcessCombatInputUseCase;
 import Fight_club.Fight_Services.Domain.models.Enums.FighterAction;
+import Fight_club.Fight_Services.Infrastructure.Inbound.DTO.Socket.PlayerInputDto;
 import lombok.AllArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -16,8 +17,8 @@ public class FightSocketController {
 
 
     @MessageMapping("/fight/{fightId}/input")
-    public void handlePlayerInput(@DestinationVariable String fightId, String userId, @Payload String action) {
-        processCombatInputUseCase.handlePlayerInput(fightId,userId, FighterAction.valueOf(action));
+        public void handlePlayerInput(@DestinationVariable String fightId, @Payload PlayerInputDto input) {
+        processCombatInputUseCase.handlePlayerInput(fightId,input.getUserId(), FighterAction.valueOf(input.getAction()));
     }
 
 }
