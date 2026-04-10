c53c7d1ba9b9a01841310a6ce2a0742421b335a9
Juan Pablo Caballero <juan.ccastellanos@mail.escuelaing.edu.co>
2026-04-09 22:30:57 -0500

    hotfix: corrección rabbit v2

--- src/main/java/Fight_club/Fight_Services/Infrastructure/Config/RabbitConfig.java
+++ src/main/java/Fight_club/Fight_Services/Infrastructure/Config/RabbitConfig.java
@@ -21,36 +21,11 @@ public class RabbitConfig {
     
     public static final String QUEUE_GUEST_REGISTERED = "fights.guest.registered.queue";
     public static final String ROUTING_KEY_GUEST = "user.guest.registered";
-    
 
-    @Bean
-    public Queue guestRegisteredQueue() {
-        return new Queue(QUEUE_GUEST_REGISTERED, true);
-    }
-
-    @Bean
-    public Binding bindingGuestRegistered(Queue guestRegisteredQueue, TopicExchange userExchange) {
-        return BindingBuilder
-                .bind(guestRegisteredQueue)
-                .to(userExchange)
-                .with(ROUTING_KEY_GUEST);
-    }
+    // EXCHANGES (Deben coincidir con el Lobby)
     @Bean
     public TopicExchange roomExchange() {
         return new TopicExchange(EXCHANGE_ROOM);
-    }
-
-    @Bean
-    public Queue roomQueue() {
-        return new Queue(ROOM_QUEUE, false);
-    }
-
-    @Bean
-    public Binding bindingInitializedRoom(Queue roomQueue, TopicExchange roomExchange) {
-        return BindingBuilder
-                .bind(roomQueue)
-                .to(roomExchange)
-                .with(ROUTING_KEY_ROOM);
     }
 
     @Bean
@@ -59,16 +34,33 @@ public class RabbitConfig {
     }
 
     @Bean
+    public Queue roomQueue() {
+        return new Queue(ROOM_QUEUE, false); 
+    }
+
+    @Bean
     public Queue userRegisteredQueue() {
         return new Queue(QUEUE_USER_REGISTERED, true); 
     }
 
     @Bean
-    public Binding bindingUserRegistered(Queue userRegisteredQueue, TopicExchange userExchange) {
-        return BindingBuilder
-                .bind(userRegisteredQueue)
-                .to(userExchange)
-                .with(ROUTING_KEY_USER_REG);
+    public Queue guestRegisteredQueue() {
+        return new Queue(QUEUE_GUEST_REGISTERED, true);
+    }
+
+    @Bean
+    public Binding bindingInitializedRoom(Queue roomQueue, TopicExchange roomExchange) {
+        return BindingBuilder.bind(roomQueue).to(roomExchange).with(ROUTING_KEY_ROOM);
+    }
+
+    @Bean
+    public Binding bindingGuestRegistered(Queue guestRegisteredQueue, DirectExchange userExchange) {
+        return BindingBuilder.bind(guestRegisteredQueue).to(userExchange).with(ROUTING_KEY_GUEST);
+    }
+
+    @Bean
+    public Binding bindingUserRegistered(Queue userRegisteredQueue, DirectExchange userExchange) {
+        return BindingBuilder.bind(userRegisteredQueue).to(userExchange).with(ROUTING_KEY_USER_REG);
     }
 
     @Bean
